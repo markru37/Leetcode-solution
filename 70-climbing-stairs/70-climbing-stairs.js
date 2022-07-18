fb0d366eba8c9ff56455a67f0c3199aea3119af2
Mark <71587458+markru37@users.noreply.github.com>
2022-07-18 18:26:48 +0300

    Time: 97 ms (28.60%), Space: 42.2 MB (18.28%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const arr = [];
+    arr.push(1);
+    arr.push(2);
+    for (let i = 2; i < n; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+    return arr[n - 1];
+};
